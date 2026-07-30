13144e1a57512a6a20fe37b4b39b6779455cb214
johncarlo-mgn <maganajohncarlodelossantos@gmail.com>
2026-07-31 00:33:57 +0800

    Set-up shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 // import { useState } from 'react'
 import './App.css'
+import { Button } from "@/components/ui/button"
+
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -7,6 +9,10 @@ function App() {
   return (
     <>
       <h1 className='text-5xl font-bold'>Hello nihunggoy</h1>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
+
     </>
   )
 }
